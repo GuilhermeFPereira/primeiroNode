a1bc211f17e4792a7ffc1c0576a79a7758b1f2c9
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-04-04 19:57:45 -0300

    atualizacao 04 04

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+
+const { request, responde} = require("express")
 const express = require('express') // primeiro , importa o express, banco de dados para dentro
-const uuid = require('uuid') // biblioteca
+const uuid = require('uuid') // biblioteca 
+const cors = require("cors")
+
+ /* import express from "express";
+import { v4 } from "uuid"; 
+import cors from "cors" */
+
 
 /*  - Query params => meusite.com/users?name=rodolfo&age=28  // usa para FILTROS // em PAGINACAO
     - Route params => /users/   // BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECIFICO
@@ -13,9 +21,11 @@ const uuid = require('uuid') // biblioteca
     - Middleware => INTERCEPTADOR => Tem o poder de parar ou alterar dados da requisicao
 */    
 
-const port = 3000
-const app = express() // maneira de facilitar o manuzeio, toda vez q quiser usar o express , utiliza essa formula -- pode ser : app ou server
-app.use(express.json()) // essa configuracao sempre tem q vir antes das rotas
+const port = 3001;
+const app = express(); // maneira de facilitar o manuzeio, toda vez q quiser usar o express , utiliza essa formula -- pode ser : app ou server
+app.use(express.json()); // essa configuracao sempre tem q vir antes das rotas
+app.use(cors())
+
 /*
 app.get('/users/:id', (request, response) => { // metedo get , para enviar uma informacao // app.get('/users', (request, response)=>{}) -- metodo padrao utilizado
   // const name = request.query.name
@@ -31,35 +41,35 @@ app.get('/users/:id', (request, response) => { // metedo get , para enviar uma i
  // return response.json({name: name, age: age})
 }) */
 
-const users = []
+const users = [];
 
 const checkUserId = (request, response, next) => {
   const { id } = request.params
 
-  const index = users.findIndex( user => user.id === id)
+  const index = users.findIndex((user)  => user.id === id)
 
   if(index < 0){ // menor que 0 , pq quando encontra uma informacao ele manda o -1
     return response.status(404).json({message:"User not found"}) // status 404 eh nao encontrado
   } 
 
-  request.userIndex = index
+  request.userIndex = index;
 
-  request.userId = id
+  request.userId = id;
 
-  next()
-}
+  next();
+};
 
-app.get('/users',(request, response)=>{
+app.get("/users",(request, response)=>{
 
-  return response.json(users)
-})
+  return response.json(users);
+});
 
-app.post('/users', (request, response)=>{
-  const { name, age } = request.body
+app.post("/users", (request, response)=>{
+  const { name, age } = request.body;
 
-  const user = {id:uuid.v4(), name,age } // uuid.v4() -- ele cria um id unico , universal
+  const user = {id: uuid.v4(), name, age }; // uuid.v4() -- ele cria um id unico , universal
 
-  users.push(user)
+  users.push(user);
 
   return response.status(201).json(user)
 })
